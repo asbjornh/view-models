d2b798b96c4e4f48b32a89ed3609e57a0b267cf9
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-11-14 15:23:14 +0100

    Removes useless comments

--- eslint-plugin/rules/no-errors/index.js
+++ eslint-plugin/rules/no-errors/index.js
@@ -8,7 +8,6 @@ module.exports = {
     const [options = {}] = context.options;
     if (!matchesFile(context.getFilename(), options.include)) return {};
 
-    // The exported visitor functions gather information about propTypes, viewModelMeta and file exports. The 'Program:exit' visitor will execute last. When it runs, the validate function is called with the gathered data. 'validate' will report any errors to eslint via the 'context' object.
     return viewModelsVisitor(getState => ({
       "Program:exit": node => {
         const { exportDeclarations, metaTypes, propTypes } = getState();
--- eslint-plugin/rules/no-unused-meta/index.js
+++ eslint-plugin/rules/no-unused-meta/index.js
@@ -10,7 +10,6 @@ module.exports = {
     const [options = {}] = context.options;
     if (!matchesFile(context.getFilename(), options.include)) return {};
 
-    // The exported visitor functions gather information about propTypes, viewModelMeta and file exports. The 'Program:exit' visitor will execute last. When it runs, the validate function is called with the gathered data. 'validate' will report any errors to eslint via the 'context' object.
     return viewModelsVisitor(getState => ({
       "Program:exit": () => {
         const { metaTypes, propTypes } = getState();
