1e0ccc5fe8368625866f5bbfd6b8480de2b80e15
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2020-06-22 19:22:40 +0200

    Makes cli test less affected by side effects

--- cli/cli.test.js
+++ cli/cli.test.js
@@ -3,15 +3,21 @@ const { exec } = require("child_process");
 const path = require("path");
 const test = require("ava");
 
+const outDir = path.resolve(__dirname, "../dist/cli-test");
+
+test.afterEach(() => {
+  fs.rmdirSync(outDir, { recursive: true });
+});
+
 test.cb("CLI", t => {
   exec(
-    "node cli --out=dist --generator=typescript --parser=typescriptReact fixtures/**/*.tsx",
+    "node cli --out=dist/cli-test --generator=typescript --parser=typescriptReact fixtures/**/*.tsx",
     err => {
       if (err) {
         t.fail(err.message);
         return;
       }
-      const files = fs.readdirSync(path.resolve(__dirname, "../dist"));
+      const files = fs.readdirSync(outDir);
       t.is(3, files.length);
       t.end();
     }
